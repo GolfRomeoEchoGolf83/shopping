9aa24ea879a9ef9389fce6dd35abf8f119845fac
GolfRomeoEchoGolf83 <gregdecroix@gmail.com>
2017-01-09 17:03:56 +0100

    creating addItems() and getItems()

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,5 +1,30 @@
 angular.module('shopping')
 
-.controller('ShoppingCtrl', function($scope) {
+.controller('ShoppingCtrl', function($scope, $http) {
+var url = 'https://shopping-c7305.firebaseio.com/items.json';
 
+$scope.items = getItems();
+
+$scope.addItems = function () {
+    var name = prompt("Que devez vous acheter ?");
+    if(name) {
+        var postData = {
+            "name": name
+        };
+        $http.post(url, postData).success(function(data) {
+            $scope.items = getItems();
+        });
+    }
+};
+
+function getItems() {
+    var items = [];
+    $http.get(url).success(function (data) {
+        angular.forEach(data, function (value, key) {
+            var name = {name: value.name};
+            items.push(name);
+        });
+    });
+    return items;
+}
 });
